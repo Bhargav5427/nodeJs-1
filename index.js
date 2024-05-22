ced0a93de6f288e1172117995136f0dc3cfd4328
Bhargav5427 <vadodariyabhargav54@gmail.com>
2024-05-22 09:22:10 +0530

    9:22

--- index.js
+++ index.js
@@ -2,122 +2,126 @@
 // console.log(name);
 
 const { log } = require('console');
-
 let http = require('http')
 
 http.createServer((req, res)=>{
-    res.end("bhargav")
+    res.end("bhargav1")
 }).listen(3001, () => {
   console.log("server is running on port 3001");
 });
 
 http.createServer((req, res)=>{
-    res.end("bhargav")
+    res.end("bhargav2")
 }).listen(3002, () => {
   console.log("server is running on port 3001");
 });
 
 http.createServer((req, res)=>{
-    res.end("bhargav")
+    res.end("bhargav3")
 }).listen(3003, () => {
   console.log("server is running on port 3001");
 });
 
 http.createServer((req, res)=>{
-    res.end("bhargav")
+    res.end("bhargav4")
 }).listen(3004, () => {
   console.log("server is running on port 3001");
 });
 
 http.createServer((req, res)=>{
-    res.end("bhargav")UGP
+    res.end("bhargav5")
 }).listen(3005, () => {
   console.log("server is running on port 3001");
 });
 
 http.createServer((req, res)=>{
-    res.end("bhargav")
+    res.end("bhargav6")
 }).listen(3006, () => {
   console.log("server is running on port 3001");
 });
 
 http.createServer((req, res)=>{
-    res.end("bhargav")
+    res.end("bhargav7")
 }).listen(3007, () => {
   console.log("server is running on port 3001");
 });
 
 http.createServer((req, res)=>{
-    res.end("bhargav")
+    res.end("bhargav8")
 }).listen(3008, () => {
+  console.log("server is running on port 3001");
+});
+http.createServer((req, res)=>{
+    res.end("bhargav9")
+}).listen(3009, () => {
   console.log("server is running on port 3001");
 });
 
 http.createServer((req, res)=>{
-    res.end("bhargav")
+    res.end("bhargav10")
 }).listen(3010, () => {
   console.log("server is running on port 3001");
 });
 
 http.createServer((req, res)=>{
-    res.end("bhargav")
+    res.end("bhargav11")
 }).listen(3011, () => {
   console.log("server is running on port 3001");
 });
 
 http.createServer((req, res)=>{
-    res.end("bhargav")
+    res.end("bhargav12")
 }).listen(3012, () => {
   console.log("server is running on port 3001");
 });
 
 http.createServer((req, res)=>{
-    res.end("bhargav")
+    res.end("bhargav13")
 }).listen(3013, () => {
   console.log("server is running on port 3001");
 });
 
 http.createServer((req, res)=>{
-    res.end("bhargav")
-}).listen(3014, () => {
+    res.end("bhargav14")
+}).listen(3014, () =>  {
   console.log("server is running on port 3001");
 });
 
 http.createServer((req, res)=>{
-    res.end("bhargav")
+    res.end("bhargav15")
 }).listen(3015, () => {
   console.log("server is running on port 3001");
 });
 
 http.createServer((req, res)=>{
-    res.end("bhargav")
+    res.end("bhargav16")
 }).listen(3016, () => {
   console.log("server is running on port 3001");
 });
 
 http.createServer((req, res)=>{
-    res.end("bhargav")
+    res.end("bhargav17")
 }).listen(3017, () => {
   console.log("server is running on port 3001");
 });
 
 
 http.createServer((req, res)=>{
-    res.end("bhargav")
+    res.end("bhargav18")
 }).listen(3018, () => {
   console.log("server is running on port 3001");
 });
 
 
 http.createServer((req, res)=>{
-    res.end("bhargav")
+    res.end("bhargav19")
 }).listen(3019, () => {
   console.log("server is running on port 3001");
 });
 
 
 http.createServer((req, res)=>{
-    res.end("bhargav")
+    res.end("bhargav20")
 }).listen(3020, () => {
   console.log("server is running on port 3001");
 });
